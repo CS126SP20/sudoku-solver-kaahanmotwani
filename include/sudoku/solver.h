a67dba8479ea19b053b06b0eb900114d793b82c0
David Brewster <brewcrew5498@outlook.com>
2020-03-12 22:41:18 -0500

    Initial commit

---
+++ include/sudoku/solver.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2020 [Your Name]. All rights reserved.
+
+#ifndef SUDOKU_SOLVER_H_
+#define SUDOKU_SOLVER_H_
+
+
+namespace sudoku {
+
+}  // namespace sudoku
+
+#endif  // SUDOKU_SOLVER_H_
+
---
+++ src/solver.cc
@@ -0,0 +1,9 @@
+// Copyright 2020 [Your Name]. All rights reserved.
+
+#include <sudoku/solver.h>
+
+
+namespace sudoku {
+
+}  // namespace sudoku
+
