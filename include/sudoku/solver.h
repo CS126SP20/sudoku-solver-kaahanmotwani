d9d474e4a5e6b47b6180b2ebac6628ba3f72f095
Kaahan <kaahan15@gmail.com>
2020-03-24 19:30:05 -0500

    Add documentation for solver.h class algorithm

--- include/sudoku/solver.h
+++ include/sudoku/solver.h
@@ -7,32 +7,34 @@
 #include <iostream>
 #include <vector>
 
+#define kRowColSize 9
+// UNASSIGNED is used for empty cells in sudoku grid
+#define UNASSIGNED 0
+
 namespace sudoku {
   using std::vector;
 
-  #define N 9
-
   /**
    * This function finds an entry in grid that is still unassigned
-   * @param grid
-   * @param row
-   * @param col
-   * @return
+   *
+   * @param grid the 9x9 integer array representing the board
+   * @param row the index of the row in the grid
+   * @param col the index of the col in the grid
+   * @return bool of if there are assigned entries left on the board
    */
-  bool FindUnassignedLocation(int grid[N][N], int &row, int &col);
+  bool FindUnassignedLocation(int grid[kRowColSize][kRowColSize],
+      int &row, int &col);
 
   /**
-   * Checks whether it will be legal to assign num to the given row, col
+   * Checks whether it will be legal to assign integer to the given row, col
    *
-   * @param grid
-   * @param row
-   * @param col
-   * @param num
-   * @return
+   * @param grid the 9x9 integer array representing the board
+   * @param row the index of the row in the grid
+   * @param col the index of the column in the grid
+   * @param num the number we are trying to assign
+   * @return a bool of if it is safe to assign num to index of the grid
    */
-  bool IsSafe(int grid[N][N], int row, int col, int num);
-
-
+  bool IsSafe(int grid[kRowColSize][kRowColSize], int row, int col, int num);
 
   /**
    * Takes a partially filled-in grid and attempts to assign values to all
@@ -41,45 +43,46 @@ namespace sudoku {
    * Algorithm for solving Sudoku puzzle below derived from:
    * https://www.geeksforgeeks.org/sudoku-backtracking-7/
    *
-   * @param grid
-   * @return
+   * @param grid the 9x9 integer array representing the board
+   * @return a bool indicating if solving the Sudoku board was a success or not
    */
-  bool SolveSudoku(int grid[N][N]);
+  bool SolveSudoku(int grid[kRowColSize][kRowColSize]);
 
   /**
    * Returns a boolean which indicates whether an assigned entry in the
    * specified row matches the given number.
    *
-   * @param grid
-   * @param row
-   * @param num
-   * @return
+   * @param grid the 9x9 integer array representing the board
+   * @param row the index of the row in the grid
+   * @param num the number we are trying to assign
+   * @return a bool of if num was used in the row
    */
-  bool UsedInRow(int grid[N][N], int row, int num);
+  bool UsedInRow(int grid[kRowColSize][kRowColSize], int row, int num);
 
   /**
    * Returns a boolean which indicates whether an assigned entry in the
    * specified column matches the given number.
-   * @param grid
-   * @param col
-   * @param num
-   * @return
+   *
+   * @param grid the 9x9 integer array representing the board
+   * @param col the index of the column in the grid
+   * @param num the number we are trying to assign
+   * @return a bool of if num was used in the column
    */
-  bool UsedInCol(int grid[N][N], int col, int num);
+  bool UsedInCol(int grid[kRowColSize][kRowColSize], int col, int num);
 
   /**
+   * Returns a boolean which indicates whether an assigned entry in the
+   * specified box (3x3) matches the given number.
    *
-   * @param grid
-   * @param boxStartRow
-   * @param boxStartCol
-   * @param num
-   * @return
+   * @param grid the 9x9 integer array representing the board
+   * @param boxStartRow the index of the start of the row in the box
+   * @param boxStartCol the index of the start of the column in the box
+   * @param num given integer that should match the assigned integer
+   * @return a boolean of whether the assigned entry in a box matches num
    */
-  bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num);
+  bool UsedInBox(int grid[kRowColSize][kRowColSize], int boxStartRow,
+      int boxStartCol, int num);
 
-
-
-  void PrintGrid(int grid[N][N]);
 }  // namespace sudoku
 
 #endif  // SUDOKU_SOLVER_H_
--- src/solver.cc
+++ src/solver.cc
@@ -4,75 +4,52 @@
 
 namespace sudoku {
 
-
-  // UNASSIGNED is used for empty cells in sudoku grid
-  #define UNASSIGNED 0
-
-  // N is used for the size of Sudoku grid.
-  // Size will be NxN
-  #define N 9
-
-  bool SolveSudoku(int grid[N][N]) {
+  bool SolveSudoku(int grid[kRowColSize][kRowColSize]) {
     int row, col;
 
-    // If there is no unassigned location,
-    // we are done
+    // If there is no unassigned location, puzzle is solved
     if (!FindUnassignedLocation(grid, row, col))
-      return true; // success!
+      return true;
 
-    // consider digits 1 to 9
-    for (int num = 1; num <= 9; num++)
-    {
-      // if looks promising
-      if (IsSafe(grid, row, col, num))
-      {
+    for (int num = 1; num <= 9; num++) {
+      if (IsSafe(grid, row, col, num)) {
         // make tentative assignment
         grid[row][col] = num;
-
-        // return, if success, yay!
+        // return, if success, the puzzle is solved
         if (SolveSudoku(grid))
           return true;
-
-        // failure, unmake & try again
+        // failure, undo the assignment & re-attempt
         grid[row][col] = UNASSIGNED;
       }
     }
-    return false; // this triggers backtracking
+    return false;
   }
 
-  /* Searches the grid to find an entry that is
-  still unassigned. If found, the reference
-  parameters row, col will be set the location
-  that is unassigned, and true is returned.
-  If no unassigned entries remain, false is returned. */
-  bool FindUnassignedLocation(int grid[N][N],
-                              int &row, int &col)
-  {
-    for (row = 0; row < N; row++)
-      for (col = 0; col < N; col++)
+  bool FindUnassignedLocation(int grid[kRowColSize][kRowColSize],
+                              int &row, int &col) {
+    for (row = 0; row < kRowColSize; row++)
+      for (col = 0; col < kRowColSize; col++)
         if (grid[row][col] == UNASSIGNED)
           return true;
     return false;
   }
 
-  bool UsedInRow(int grid[N][N], int row, int num) {
-    for (int col = 0; col < N; col++)
+  bool UsedInRow(int grid[kRowColSize][kRowColSize], int row, int num) {
+    for (int col = 0; col < kRowColSize; col++)
       if (grid[row][col] == num)
         return true;
     return false;
   }
 
-  bool UsedInCol(int grid[N][N], int col, int num) {
-    for (int row = 0; row < N; row++)
+  bool UsedInCol(int grid[kRowColSize][kRowColSize], int col, int num) {
+    for (int row = 0; row < kRowColSize; row++)
       if (grid[row][col] == num)
         return true;
     return false;
   }
 
-  /* Returns a boolean which indicates whether
-  an assigned entry within the specified 3x3 box
-  matches the given number. */
-  bool UsedInBox(int grid[N][N], int boxStartRow, int boxStartCol, int num) {
+  bool UsedInBox(int grid[kRowColSize][kRowColSize], int boxStartRow,
+      int boxStartCol, int num) {
     for (int row = 0; row < 3; row++)
       for (int col = 0; col < 3; col++)
         if (grid[row + boxStartRow]
@@ -81,12 +58,9 @@ namespace sudoku {
     return false;
   }
 
-  /* Returns a boolean which indicates whether
-  it will be legal to assign num to the given
-  row, col location. */
-  bool IsSafe(int grid[N][N], int row, int col, int num) {
-    /* Check if 'num' is not already placed in
-    current row, current column and current 3x3 box */
+  bool IsSafe(int grid[kRowColSize][kRowColSize], int row, int col, int num) {
+    // Checks if 'num' is not already placed in
+    // current row, current column and current 3x3 box
     return !UsedInRow(grid, row, num) &&
            !UsedInCol(grid, col, num) &&
            !UsedInBox(grid, row - row % 3 ,
@@ -94,14 +68,5 @@ namespace sudoku {
            grid[row][col] == UNASSIGNED;
   }
 
-  /* A utility function to print grid */
-  void PrintGrid(int grid[N][N]) {
-    for (int row = 0; row < N; row++) {
-      for (int col = 0; col < N; col++) {
-        std::cout << grid[row][col] << " ";
-      }
-      std::cout << std::endl;
-    }
-  }
 }  // namespace sudoku
 
